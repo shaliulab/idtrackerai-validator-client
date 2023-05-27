a4a69103cef612b16fd47224ad1bc8d15b3fca41
antortjim <ntoniohu@gmail.com>
2023-05-27 14:41:52 +0000

    FIrst commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [frame, setFrame] = useState(null);
+  const [frameNumber, setFramenumber] = useState(2250000); // Default frame number
+  
+  const fetchFrame = () => {
+    axios.get(`http://localhost:5000/api/frame/${frameNumber}`,  { responseType: 'blob' })
+      .then(response => {
+        // Here you should handle the response appropriately.
+        // If your server responds with a JSON, the following line should work fine:
+        console.log(response.data);
+        // If your server responds with an image, you might want to display the image instead:
+        setFrame(URL.createObjectURL(response.data));
+      })
+      .catch(error => {
+        console.error("Error fetching frame: ", error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="range" min="2250000" max="15750000" value={frameNumber} onChange={e => setFramenumber(e.target.value)} />
+    <p>Current frame: {frameNumber}</p>
+    <button onClick={fetchFrame}>Fetch Frame</button>
+    {frame && <img src={frame} alt="Frame" />}
     </div>
   );
 }
