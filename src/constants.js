a018ff9a50a3840929003c62890f92e9bde75502
antortjim <ntoniohu@gmail.com>
2023-06-21 17:19:00 +0000

    Extend time limits of experiments

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const CHUNKSIZE=45000
 export const FRAMERATE=150;
-export const MAX_FN=CHUNKSIZE*350-1;
-export const MIN_FN=CHUNKSIZE*50;
+export const MAX_FN=CHUNKSIZE*450-1;
+export const MIN_FN=CHUNKSIZE*0;
 export const BACKEND_SERVER="10.43.205.15";
 // export const BACKEND_SERVER="192.168.8.160";
