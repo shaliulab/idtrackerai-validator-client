1dc64a5ca0519a6e49fc4b992b95185547608484
antortjim <ntoniohu@gmail.com>
2024-01-29 09:55:12 +0000

    Implement router

--- src/myRouter.js
+++ src/myRouter.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { BACKEND_SERVER } from './constants'
 
 function MyRouter() {
     let { datasetName } = useParams();
-    fetch(`http://localhost:5000/api/get/${datasetName}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(response => response.json())
-    // .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    console.log("Fetching to api/get");
+
+    useEffect(() => {
+        console.log("Fetching to api/get");
+        fetch(`http://${BACKEND_SERVER}:5000/api/get/${datasetName}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+    }, [datasetName]); // Dependency array
 
 
     return <div>Loading dataset ${datasetName}...</div>;
