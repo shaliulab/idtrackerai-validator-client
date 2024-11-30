3074648065f3a05174708124565eaaaec219f3bb
antortjim <ntoniohu@gmail.com>
2024-11-30 07:46:59 +0000

    Respond to videoFrameRate changes

--- src/scrollableNumber.js
+++ src/scrollableNumber.js
@@ -9,10 +9,18 @@ const ScrollNumberInput = ({ value, setValue, videoFrameRate, id, labelText, foc
         }, 100)
     ).current;
 
+    // Store the latest videoFrameRate in a ref
+    const videoFrameRateRef = useRef(videoFrameRate);
+
+    useEffect(() => {
+        // Update the ref whenever videoFrameRate changes
+        videoFrameRateRef.current = videoFrameRate;
+    }, [videoFrameRate]);
+
     useEffect(() => {
         const handleWheel = (event) => {
             event.preventDefault();
-            const delta = event.deltaY < 0 ? videoFrameRate : -videoFrameRate;
+            const delta = event.deltaY < 0 ? videoFrameRateRef.current : -videoFrameRateRef.current;
 
             // Pass the update function to the debounced handler
             debouncedSetValue((prevValue) => prevValue + delta);
@@ -29,7 +37,7 @@ const ScrollNumberInput = ({ value, setValue, videoFrameRate, id, labelText, foc
             }
             debouncedSetValue.cancel(); // Clean up debounce on unmount
         };
-    }, [debouncedSetValue, focusElementRef]);
+    }, [debouncedSetValue, focusElementRef]); // No need to include videoFrameRate here
 
     return (
         <label htmlFor={id}>
@@ -44,6 +52,4 @@ const ScrollNumberInput = ({ value, setValue, videoFrameRate, id, labelText, foc
     );
 };
 
-
 export default ScrollNumberInput;
-
