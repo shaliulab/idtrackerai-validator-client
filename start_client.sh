3e3aab6b73330f7e9e59eed27f9112dc63ff2644
antortjim <ntoniohu@gmail.com>
2026-06-05 16:21:37 +0000

    Update start client

--- start_client.sh
+++ start_client.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd /home/vibflysleep/opt/idtrackerai-validator-client
-/home/vibflysleep/opt/node-v18.16.0-linux-x64/bin/npm start
+REACT_APP_BACKEND_PORT=5000 /home/vibflysleep/opt/node-v18.16.0-linux-x64/bin/npm start
